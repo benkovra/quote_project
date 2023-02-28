a8b40ea74670c5c10e09b4b80259da58debe4704
benkovra <112646826+benkovra@users.noreply.github.com>
2023-02-28 14:10:32 -0500

    added quotes

--- data.js
+++ data.js
@@ -1,4 +1,67 @@
 const data = 
 [{
-    
-}]
+    "text": "Try not. Do or do not. There is no try.",
+    "author": "Yoda",
+    "trilogy": "original"
+},
+{
+    "text": "Your eyes can deceive you; don’t trust them.",
+    "author": "obi wan kenobi",
+    "trilogy": "original"
+},
+{
+    "text": "Who’s the more foolish: the fool or the fool who follows him?",
+    "author":"obi wan kenobi",
+    "trilogy": "original"
+},
+{
+    "text": "Your focus determines your reality.",
+    "author": "Qui-Gon Jinn",
+    "trilogy": "prequels"
+},
+{
+    "text": "No longer certain that one ever does win a war, I am",
+    "author": "Yoda",
+    "trilogy": "clone wars"
+},
+{
+    "text": "In a dark place we find ourselves and a little more knowledge lights our way.",
+    "author": "Yoda",
+    "trilogy": "prequels"
+},
+{
+    "text": "Sometimes we must let go of our pride and do what is requested of us.",
+    "author": "Anakin Skywalker",
+    "trilogy": "prequels"
+},
+{
+    "text": "We’ll always be with you. No one’s ever really gone. A thousand generations live in you now.",
+    "author": "Luke Skywalker",
+    "trilogy": "sequels"
+},
+{
+    "text": "The ability to speak does not make you intelligent.",
+    "author": "Qui-Gon Jinn",
+    "trilogy": "prequels"
+},
+{
+    "text": "Many of the truths that we cling to depend on our viewpoint.",
+    "author": "Obi wan Kenobi",
+    "trilogy": "original"
+},
+{
+    "text": "Great, kid, don’t get cocky.",
+    "author": "Han Solo",
+    "trilogy": "original"
+},
+{
+    "text": "Be careful not to choke on your aspirations, director.",
+    "author": "Darth Vader",
+    "trilogy": "Rogue One"
+},
+{
+    "text": "Let go of your hate.",
+    "author": "Luke Skywalker",
+    "trilogy": "original"
+},
+]
