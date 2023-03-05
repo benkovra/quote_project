872cb4c939e3fedbd76122a025a1c1b895842cfe
benkovra <112646826+benkovra@users.noreply.github.com>
2023-03-05 16:28:06 -0500

    fixed buttons

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-function logMe(){
-    console.log(data)
-}
+// function logMe(){
+//     console.log(data)
+// }
 
-let button = document.querySelector('button');
-button.addEventListener('click', () => {
+// let button = document.querySelector('button');
+// button.addEventListener('click', () => {
+//     let i = Math.floor(Math.random() * data.length);
+//     let quote = document.querySelector('blockquote');
+//     let cite = document.querySelector('figcaption');
+//     quote.innerText = data[i].text;
+//     cite.innerText = data[i].cite;
+// })
+function getCat() {
     let i = Math.floor(Math.random() * data.length);
     let quote = document.querySelector('blockquote');
     let cite = document.querySelector('figcaption');
     quote.innerText = data[i].text;
     cite.innerText = data[i].cite;
-})
+    
+    console.log(i);
+}    
