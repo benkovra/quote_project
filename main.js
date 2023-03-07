dbcbf454a967ac80e406e60fb90e0a21a68e38c8
benkovra <112646826+benkovra@users.noreply.github.com>
2023-03-07 15:44:04 -0500

    fix category

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-// function logMe(){
-//     console.log(data)
-// }
 
-// let button = document.querySelector('button');
-// button.addEventListener('click', () => {
-//     let i = Math.floor(Math.random() * data.length);
-//     let quote = document.querySelector('blockquote');
-//     let cite = document.querySelector('figcaption');
-//     quote.innerText = data[i].text;
-//     cite.innerText = data[i].cite;
-// })
 function getCat() {
     let i = Math.floor(Math.random() * data.length);
     let quote = document.querySelector('blockquote');
